373564e488863cfac1714b106ccc5347deb8f674
CyanogenPlayer <superkriper45@gmail.com>
2023-08-17 21:40:21 +0300

    lesson 2

---
+++ lesson2/main.js
@@ -0,0 +1,96 @@
+// let groups = [
+//     'acdc',
+//     'imagine dragons',
+//     'linkin park',
+//     'godsmack'
+// ];
+//
+// console.log(groups);
+// console.log(groups[0]);
+// console.log(groups.length);
+// let lastIndex = groups.length - 1;
+// console.log(groups[lastIndex]);
+//
+// groups[4] = 'bad wolves';
+// console.log(groups);
+// groups.push('halestorm');
+// console.log(groups);
+// groups[groups.length] = '????';
+// console.log(groups);
+
+// let musicFolder = [
+//     ['queen', 'acdc', 'queen'],
+//     ['eminem', '2pac'],
+//     ['awd', 'dadsa', 'aa']
+// ];
+//
+// console.log(musicFolder.length);
+// console.log(musicFolder[0][2]);
+
+// let user = {
+//     id: 1,
+//     name: 'vasya',
+//     age: 31,
+//     status: true
+// };
+//
+// console.log(user);
+// console.log(user.id);
+// console.log(user['id']);
+//
+// // let users = [
+// //     {id: 1, name: 'ein', age: 69},
+// //     {id: 2, name: 'zwei', age: 666},
+// //     {id: 3, name: 'drei', age: 555},
+// //     {id: 4, name: 'vier', age: 2}
+// // ];
+// //
+// // console.log(users);
+//
+// console.log(user);
+// user['skills'] = ['js', 'css', 'python'];
+// console.log(user)
+
+// let p = 0;
+// let pCopy = p;
+// pCopy = 1;
+//
+// console.log(p);
+// console.log(pCopy);
+
+// let origin = {};
+// let clone = origin;
+//
+// clone.id = 100500;
+// console.log(clone);
+// console.log(origin);
+
+// if (confirm('???')) {
+//     console.log('hello');
+// } else {
+//     console.log('bye');
+// }
+
+// let age = +prompt('enter age');
+// if (age > 18) {
+//     console.log('adult');
+// } else {
+//     console.log('cartoon');
+// }
+
+// let color = prompt('color');
+// if (color === 'g') {
+//     console.log('go');
+// } else if (color === 'y') {
+//     console.log('wait');
+// } else if (color === 'r') {
+//     console.log('stop');
+// } else {
+//     console.log('????');
+// }
+
+// let x = 5 > 6 ? 100 : 200;
+// console.log(x);
+
+let name = prompt("name?") || "john";
+console.log(name);
