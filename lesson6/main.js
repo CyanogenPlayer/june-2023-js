1a6c5f5d62102f80824abae9a3b1f3f4c3cd4e09
Nazarii Ryhus <superkriper45@gmail.com>
2023-09-04 08:46:23 +0300

    lesson 6

---
+++ lesson6/main.js
@@ -0,0 +1,45 @@
+// let str = 'Hello, world';
+//
+// // console.log(str.concat('!'));
+//
+// // console.log(str.toUpperCase());
+// // console.log(str.toLowerCase());
+//
+// // console.log(str.startsWith('Hell'));
+// // console.log(str.startsWith('hell'));
+// //
+// // console.log(str.endsWith('rld'));
+//
+// // console.log(str.substring(0, 8));
+// // console.log(str.indexOf('o'));
+//
+// // console.log(str.split(' '));
+//
+// // console.log(str.replaceAll(',', ''));
+
+// let arr = [];
+//
+// let newSizeOfArray = arr.push('asd');
+// console.log(arr, newSizeOfArray);
+//
+// arr.unshift('dsa');
+// console.log(arr);
+//
+// let shifted = arr.shift();
+// console.log(arr, shifted);
+//
+// let poppedElement = arr.pop();
+// console.log(arr, poppedElement);
+
+let arr = [11, 22, 33, 44];
+
+// console.log(arr.join('-'));
+
+// console.log(arr.concat([111, 222, 333, 444]));
+
+// console.log(arr.slice(1, 3));
+
+console.log(arr)
+// console.log(arr.splice(1, 2));
+console.log(arr.splice(1, 2, 'das', 'sad', 'asd'));
+console.log(arr);
